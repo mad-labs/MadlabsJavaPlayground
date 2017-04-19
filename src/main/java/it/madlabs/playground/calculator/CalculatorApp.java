212c6d102eb5ac0db7903006bc22d053b745b8b7
gabriele <gabriele.manfredi@hotmail.it>
2017-04-19 17:22:47 +0200

    My Solution 	modified:   src/main/java/it/madlabs/playground/calculator/CalculatorApp.java 	modified:   src/main/java/it/madlabs/playground/calculator/MattyCalculator.java 	modified:   target/maven-archiver/pom.properties 	modified:   target/surefire-reports/TEST-it.madlabs.playground.calculator.CalculatorAppTest.xml 	modified:   target/surefire-reports/it.madlabs.playground.calculator.CalculatorAppTest.txt

--- src/main/java/it/madlabs/playground/calculator/CalculatorApp.java
+++ src/main/java/it/madlabs/playground/calculator/CalculatorApp.java
@@ -34,28 +34,28 @@ public class CalculatorApp {
 		 * Your objective is to create a constructor with a string parameter in MattyCalculator.java to instantiate a new object
 		 */
 
-		// //START LEVEL -1
-		// System.out.println("Creating new instance on MattyCalculator application");
-		// MattyCalculator mattyCalculator = new MattyCalculator("Created at " + new Date());
+		//START LEVEL -1
+		System.out.println("Creating new instance on MattyCalculator application");
+		MattyCalculator mattyCalculator = new MattyCalculator("Created at " + new Date());
 
-		// //test the expected result
-		// System.out.println("Testing initialization. mattyCalculator shuld not be null!");
-		// if (mattyCalculator == null){
-		// 	throw new RuntimeException("Error!!! mattyCalculator should not be null!! Instead is: " + mattyCalculator);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
-		// //END LEVEL -1
+		//test the expected result
+		System.out.println("Testing initialization. mattyCalculator shuld not be null!");
+		if (mattyCalculator == null){
+			throw new RuntimeException("Error!!! mattyCalculator should not be null!! Instead is: " + mattyCalculator);
+		} else {
+			System.out.println("Test OK!");
+		}
+		//END LEVEL -1
 
 		/*
 		 * LEVEL 0: start our application
 		 * Your objective is to create a method in MattyCalculator.java to start our application
 		 */
 
-		// //START LEVEL 0
-		// System.out.println("Starting MattyCalculator application");
-		// mattyCalculator.start();
-		// //END LEVEL 0
+		//START LEVEL 0
+		System.out.println("Starting MattyCalculator application");
+		mattyCalculator.start();
+		//END LEVEL 0
 
 		/*
 		 * LEVEL 1: getting a result
@@ -64,17 +64,17 @@ public class CalculatorApp {
 		 * with 0
 		 */
 
-		// //START LEVEL 1
-		// double result = mattyCalculator.getResult();
+		//START LEVEL 1
+		double result = mattyCalculator.getResult();
 
-		// //test the expected result
-		// System.out.println("Testing initialization. mattyCalculator.getResult() should return 0");
-		// if (result != 0){
-		// 	throw new RuntimeException("Error!!! the result should be 0!!! Instead is: " + result);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
-		// //END LEVEL 1
+		//test the expected result
+		System.out.println("Testing initialization. mattyCalculator.getResult() should return 0");
+		if (result != 0){
+			throw new RuntimeException("Error!!! the result should be 0!!! Instead is: " + result);
+		} else {
+			System.out.println("Test OK!");
+		}
+		//END LEVEL 1
 
 		/*
 		 * LEVEL 2: sum of 2 number
@@ -82,62 +82,62 @@ public class CalculatorApp {
 		 * You will need some fields in the MattyCalculator.java to store x, y and result. Initialize them with 0
 		 */
 
-		// //START LEVEL 2
-		// mattyCalculator.setX(10);
-		// mattyCalculator.setY(5);
-		// mattyCalculator.sum();
-		// result = mattyCalculator.getResult();
+		//START LEVEL 2
+		mattyCalculator.setX(10);
+		mattyCalculator.setY(5);
+		mattyCalculator.sum();
+		result = mattyCalculator.getResult();
 
-		// //test the expected result
-		// System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.sum(), mattyCalculator.getResult() should return 15");
-		// if (result != 15){
-		// 	throw new RuntimeException("Error!!! the result should be 15!!! Instead is: " + result);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
-		// //END LEVEL 2
+		//test the expected result
+		System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.sum(), mattyCalculator.getResult() should return 15");
+		if (result != 15){
+			throw new RuntimeException("Error!!! the result should be 15!!! Instead is: " + result);
+		} else {
+			System.out.println("Test OK!");
+		}
+		//END LEVEL 2
 
 		/*
 		 * LEVEL 3: other operation
 		 * Your objective is to create other operation: subtract, multiply, divide.
 		 */
-		// //START LEVEL 3
-		// mattyCalculator.setX(10);
-		// mattyCalculator.setY(5);
+		//START LEVEL 3
+		mattyCalculator.setX(10);
+		mattyCalculator.setY(5);
 
-		// mattyCalculator.subtract();
-		// result = mattyCalculator.getResult();
+		mattyCalculator.subtract();
+		result = mattyCalculator.getResult();
 
-		// //test the expected result
-		// System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.subtract(), mattyCalculator.getResult() should return 5");
-		// if (result != 5){
-		// 	throw new RuntimeException("Error!!! the result should be 5!!! Instead is: " + result);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
+		//test the expected result
+		System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.subtract(), mattyCalculator.getResult() should return 5");
+		if (result != 5){
+			throw new RuntimeException("Error!!! the result should be 5!!! Instead is: " + result);
+		} else {
+			System.out.println("Test OK!");
+		}
 
-		// mattyCalculator.multiply();
-		// result = mattyCalculator.getResult();
+		mattyCalculator.multiply();
+		result = mattyCalculator.getResult();
 
-		// //test the expected result
-		// System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.multiply(), mattyCalculator.getResult() should return 50");
-		// if (result != 50){
-		// 	throw new RuntimeException("Error!!! the result should be 50!!! Instead is: " + result);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
+		//test the expected result
+		System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.multiply(), mattyCalculator.getResult() should return 50");
+		if (result != 50){
+			throw new RuntimeException("Error!!! the result should be 50!!! Instead is: " + result);
+		} else {
+			System.out.println("Test OK!");
+		}
 
-		// mattyCalculator.divide();
-		// result = mattyCalculator.getResult();
+		mattyCalculator.divide();
+		result = mattyCalculator.getResult();
 
-		// //test the expected result
-		// System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.divide(), mattyCalculator.getResult() should return 2");
-		// if (result != 2){
-		// 	throw new RuntimeException("Error!!! the result should be 2!!! Instead is: " + result);
-		// } else {
-		// 	System.out.println("Test OK!");
-		// }
-		// //END LEVEL 3
+		//test the expected result
+		System.out.println("Testing: given x=10 and y=5, when call mattyCalculator.divide(), mattyCalculator.getResult() should return 2");
+		if (result != 2){
+			throw new RuntimeException("Error!!! the result should be 2!!! Instead is: " + result);
+		} else {
+			System.out.println("Test OK!");
+		}
+		//END LEVEL 3
 
 		/*
 		 * LEVEL 4: interactive prompt
@@ -147,25 +147,25 @@ public class CalculatorApp {
 		 * You can use Class MattyCalculatorUtils to ask user input, to read it and show result.
 		 */
 
-		// //START LEVEL 4
-		// System.out.println("\n\n\n");
-		// System.out.println("Welcome USER! Let's do some maths! :)");
-		// System.out.println("\n\n");
-		// System.out.println("Here the instruction:");
-		// System.out.println("-  to do operation you need to insert X and Y number and after choose and operation");
-		// System.out.println("-  to exit press [CTRL + C]");
-		// System.out.println("\n");
-		// System.out.println("Enjoy the MattyClaculator! :)");
-		// System.out.println("\n\n");
+		//START LEVEL 4
+		System.out.println("\n\n\n");
+		System.out.println("Welcome USER! Let's do some maths! :)");
+		System.out.println("\n\n");
+		System.out.println("Here the instruction:");
+		System.out.println("-  to do operation you need to insert X and Y number and after choose and operation");
+		System.out.println("-  to exit press [CTRL + C]");
+		System.out.println("\n");
+		System.out.println("Enjoy the MattyClaculator! :)");
+		System.out.println("\n\n");
 
-		// boolean validOpetation = true;
-		// while(validOpetation) {
-		// 	mattyCalculator.askX();
-		// 	mattyCalculator.askY();
-		// 	mattyCalculator.askOperation();
-		// 	mattyCalculator.showResult();
-		// }
-		// //END LEVEL 4
+		boolean validOpetation = true;
+		while(validOpetation) {
+			mattyCalculator.askX();
+			mattyCalculator.askY();
+			mattyCalculator.askOperation();
+			mattyCalculator.showResult();
+		}
+		//END LEVEL 4
 
 		System.out.println("MattyCalculator application ends");
 
--- src/main/java/it/madlabs/playground/calculator/MattyCalculator.java
+++ src/main/java/it/madlabs/playground/calculator/MattyCalculator.java
@@ -10,21 +10,79 @@ import it.madlabs.playground.calculator.utils.MattyCalculatorUtils;
  */
   public class MattyCalculator {
 
-  
-    // // Hint: try to decomment code below, it could not work, but maybe it will aid you!
-    // public MattyCalculator(String creationMessage){
-    //  System.out.println("@@@ new MattyCalculator created with message: " + creationMessage + " @@@");
-    // }
+    private double x = 0d;
+    private double y = 0d;
+    private double result = 0d;
+    private MattyCalculatorUtils mattyCalculatorUtils = new MattyCalculatorUtils();
+    
+    // Hint: try to decomment code below, it could not work, but maybe it will aid you!
+    public MattyCalculator(String creationMessage){
+     System.out.println("@@@ new MattyCalculator created with message: " + creationMessage + " @@@");
+    }
 
-    // // Hint: try to decomment code below, it could not work, but maybe it will aid you!
-    // public void start(){
-    //  System.out.println("@@@ Starting MattyCalculator... @@@");
-    // }
+    // Hint: try to decomment code below, it could not work, but maybe it will aid you!
+    public void start(){
+     System.out.println("@@@ Starting MattyCalculator... @@@");
+    }
 
-    // // Hint: try to decomment code below, it could not work, but maybe it will aid you!
-    // public double getResult(){
-    //    return result;
-    // }
+    // Hint: try to decomment code below, it could not work, but maybe it will aid you!
+    public double getResult(){
+       return result;
+    }
 
+    public void setX(double x){
+      this.x = x;
+    }
 
+    public void setY(double y){
+      this.y = y;
+    }
+
+    public void sum(){
+      this.result = x + y;
+    }
+
+    public void subtract(){
+      this.result = x - y;
+    }
+
+    public void multiply(){
+      this.result = x * y;
+    }
+
+    public void divide(){
+      this.result = x / y;
+    }
+
+    public void askX(){
+      this.x = this.mattyCalculatorUtils.askADouble("Please insert X value below: ");
+    }
+
+    public void askY(){
+      this.y = this.mattyCalculatorUtils.askADouble("Please insert Y value below: ");
+    }
+
+    public void askOperation(){
+      String op = this.mattyCalculatorUtils.askAString("Please choose and opetation in '+', '-', '*', '/' and write it below: ");
+      switch (op) {
+        case "+":
+          this.sum();
+          break;
+        case "-":
+          this.subtract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "/":
+          this.divide();
+          break;
+        default:
+          throw new IllegalArgumentException("Argh! I don't know this operation! Going to kill myself!!!");
+      }
+    }
+
+    public void showResult(){
+      System.out.println(" > Result is: " + this.result);
+    }
 }
