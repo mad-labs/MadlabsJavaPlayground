60396ff0ce3f23cddad021e155cdb6b79b587c31
Gabriele <gabriele.manfredi.999@gmail.com>
2017-04-30 16:07:43 +0200

    new file:   src/main/java/it/madlabs/playground/mp3player/Mp3Player.java 	new file:   src/main/java/it/madlabs/playground/mp3player/Mp3PlayerApp.java

---
+++ src/main/java/it/madlabs/playground/mp3player/Mp3Player.java
@@ -0,0 +1,20 @@
+package it.madlabs.playground.mp3Player;
+
+public class Mp3Player {
+    
+    public void switchOn(){
+        System.out.println("wheunwehunwheun..");
+    }
+
+    public void switchOff(){
+       System.out.println("uwheuwheuwhe..");
+    }
+
+    public void play(){
+       System.out.println();
+    }
+
+    public void stop(){
+       System.out.println();
+    }
+}
---
+++ src/main/java/it/madlabs/playground/mp3player/Mp3PlayerApp.java
@@ -0,0 +1,17 @@
+package it.madlabs.playground.mp3Player;
+
+import it.madlabs.playground.mp3Player.Mp3Player;
+import java.io.IOException;
+
+public class Mp3PlayerApp {
+  
+	public static void main (String[] args) throws IOException {
+		System.out.println("Starting Mp3Player App main... ");
+
+        Mp3Player mp3Player = new Mp3Player();
+        mp3Player.switchOn();
+        mp3Player.play();
+        mp3Player.stop();
+        mp3Player.switchOff();
+    }
+}
