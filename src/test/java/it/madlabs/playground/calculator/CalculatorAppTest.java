10152de01804ecdaf8b45a2c8e498db86b5c2813
Gabriele <gabriele.manfredi.999@gmail.com>
2017-05-01 10:46:36 +0200

    added build and execute script 	new file:   build_and_execute.sh 	modified:   src/test/java/it/madlabs/playground/calculator/CalculatorAppTest.java

--- src/test/java/it/madlabs/playground/calculator/CalculatorAppTest.java
+++ src/test/java/it/madlabs/playground/calculator/CalculatorAppTest.java
@@ -1,13 +1,13 @@
 package it.madlabs.playground.calculator;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
 
 /**
  * Unit test for simple CalculatorAppTest.
  */
-public class CalculatorAppTest extends TestCase
+public class CalculatorAppTest //extends TestCase
 {
     /**
      * Create the test case
@@ -15,20 +15,20 @@ public class CalculatorAppTest extends TestCase
      * @param testName name of the test case
      */
     public CalculatorAppTest( String testName ) {
-        super( testName );
+        //super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite() {
-        return new TestSuite( CalculatorAppTest.class );
-    }
+    //public static Test suite() {
+        //return null; //new TestSuite( CalculatorAppTest.class );
+   // }
 
     /**
      * Rigourous Test :-)
      */
     public void testApp() {
-        assertTrue( true );
+        //assertTrue( true );
     }
 }
